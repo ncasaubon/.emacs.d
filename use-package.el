a7479a6843da4092febd4e16a741eb0cc56b2597
Nicolas <ncasaubon@protonmail.com>
2020-10-24 14:00:15 -0500

    Removed all commented code, replaced ivy with selectrum

--- use-package.el
+++ use-package.el
@@ -47,9 +47,6 @@
   :straight t)
 
 ;; Org
-;; (use-package org-mode
-;;   :straight (:type built-in)
-;;   :defer t)
 (use-package org-mode
   :straight (org-mode
              :repo "https://code.orgmode.org/bzg/org-mode.git")
@@ -100,19 +97,6 @@
   :config
   (projectile-mode t))
 
-;; Treemacs
-;; (use-package treemacs
-;;   :straight t
-;;   :hook prog-mode)
-
-;; (use-package treemacs-magit
-;;   :after treemacs magit
-;;   :straight t)
-
-;; (use-package treemacs-projectile
-;;   :after treemacs projectile
-;;   :straight t)
-
 ;; Dired
 (use-package dired-x
   :straight (:type built-in)
@@ -122,28 +106,27 @@
   :straight t
   :after dired)
 
-;; Ivy
-(use-package ivy
+;; Selectrum
+(use-package selectrum
   :straight t
-  :init
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t)
-  (setq search-default-mode #'char-fold-to-regexp)
-  (ivy-mode 1)
-  :bind (("C-c C-r" . ivy-resume)
-         ("<f6>" . ivy-resume)))
+  :config
+  (selectrum-mode +1))
 
-;; Swiper
-(use-package swiper
+;; CTRLF
+(use-package ctrlf
   :straight t
-  :bind (("C-s" . swiper)
-         ("C-r" . swiper)))
+  :config
+  (ctrlf-mode +1))
 
-;; Counsel
-(use-package counsel
+;; Prescient
+(use-package prescient
+  :straight t)
+
+;; Selectrum-Prescient
+(use-package selectrum-prescient
   :straight t
-  :init
-  (counsel-mode 1))
+  :config
+  (selectrum-prescient-mode +1))
 
 ;; Avy
 (use-package avy
@@ -155,24 +138,6 @@
 (use-package bufler
   :straight t
   :bind ("C-x C-b" . bufler))
-
-;; Posframe for positioning frames in preferred places.
-;; (use-package posframe
-;;   :straight t)
-
-;; Posframe ivy integration
-;; (use-package ivy-posframe
-;;   :straight t
-;;   :config
-;;   (ivy-posframe-mode t)
-;;   :custom
-;;   (ivy-posframe-display-functions-alist
-;;    '((swiper          . ivy-posframe-display-at-frame-top-center)
-;;      (complete-symbol . ivy-posframe-display-at-point)
-;;      (counsel-M-x     . ivy-posframe-display-at-frame-top-center)
-;;      (t               . ivy-posframe-display-at-frame-top-center)))
-;;   :after
-;;   (ivy posframe))
 
 ;; Ace Window
 (use-package ace-window
@@ -199,36 +164,7 @@
   :init
   (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
 
-;; Centaur tabs
-;; (use-package centaur-tabs
-;;   :straight t
-;;   :defer t
-;;   :config
-;;   (centaur-tabs-headline-match)
-;;   :hook
-;;   (term-mode . centaur-tabs-local-mode)
-;;   (calendar-mode . centaur-tabs-local-mode)
-;;   (org-agenda-mode . centaur-tabs-local-mode)
-;;   :bind
-;;   ("C-x <right>" . #'centaur-tabs-forward)
-;;   ("C-x <left>" . #'centaur-tabs-backward)
-;;   ("C-c t t" . centaur-tabs-toggle-groups)
-;;   ("C-c t s" . centaur-tabs-counsel-switch-group)
-;;   ("C-c t p" . centaur-tabs-group-by-projectile-project)
-;;   ("C-c t g" . centaur-tabs-group-buffer-groups)
-;;   :custom
-;;   (uniquify-separator "/")
-;;   (uniquify-buffer-name-style 'forward)
-;;   (centaur-tabs-style "bar")
-;;   (centaur-tabs-height 24)
-;;   (centaur-tabs-set-icons t)
-;;   (centaur-tabs-cycle-scope 'tabs)
-;;   (centaur-tabs-set-modified-marker t)
-;;   (centaur-tabs-modified-marker "•")
-;;   (x-underline-at-descent-line t)
-;;   (centaur-tabs-set-bar 'under))
-
-;; LSP basics
+;; LSP
 (use-package lsp-mode
   :straight t
   :defer t
@@ -237,10 +173,6 @@
   :straight t
   :after lsp-mode
   :commands lsp-ui-mode)
-(use-package lsp-ivy
-  :straight t
-  :after lsp ivy
-  :commands lsp-ivy-workspace-symbol)
 (use-package lsp-jedi
   :straight (lsp-jedi
              :host github
